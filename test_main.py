9424557178c8966ad130180061552e9b51919826
unknown <ex276@nyu.edu>
2022-10-11 17:14:09 +0800

    tested hole detection

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 import socket
 import time
 import struct
@@ -22,7 +23,7 @@ def get_current_pos():  # x, y, theta
     rpy = util.rv2rpy(tcp[3], tcp[4], tcp[5])
     return np.asarray([tcp[0], tcp[1], rpy[-1]])
 
-def move_to_tcp(target_tcp):
+def move_tcp_to(target_tcp):
     tool_acc = 1.2  # Safe: 0.5
     tool_vel = 0.25  # Safe: 0.2
     tool_pos_tolerance = [0.001, 0.001, 0.001, 0.05, 0.05, 0.05]
@@ -32,9 +33,9 @@ def move_to_tcp(target_tcp):
         tool_acc, tool_vel)
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.connect((HOST, PORT))
-    tcp_socket.send(str.encode(tcp_command))  # 利用字符串的encode方法编码成bytes，默认为utf-8类型
+    tcp_socket.send(str.encode(tcp_command))  # implement the encode method of string to encode command as bytes，defaults to utf-8.
     tcp_socket.close()
-    # 确保已达到目标点，就可以紧接着发送下一条指令
+    # make sure the target pose is reached，and then the next command could be sent right after that.
     actual_pos = get_current_tcp()
     target_rpy = util.rv2rpy(target_tcp[3], target_tcp[4], target_tcp[5])
     rpy = util.rv2rpy(actual_pos[3], actual_pos[4], actual_pos[5])
@@ -56,7 +57,7 @@ def increase_move(delta_x, delta_y, delta_z, delta_theta):
     target_rv = util.rpy2rv(rpy)
     target_tcp = np.asarray([tcp[0] + delta_x, tcp[1] + delta_y, tcp[2] + delta_z,
                              target_rv[0], target_rv[1], target_rv[2]])
-    move_to_tcp(target_tcp)
+    move_tcp_to(target_tcp)
 
 def get_digital_output():
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -124,44 +125,79 @@ def get_discretized_poses():
     # delta_z = 0.01
     delta_phi = 0.56
     discretized_poses = []
-    for num in range(int(2*np.pi/(delta_phi/2))):
-        x = 0.445 + 0.05 * np.sin(phi_xmax + num * delta_phi/2)
-        y = 0.736 + 0.05 * np.cos(phi_xmax + num * delta_phi/2)
+    for num in range(int(np.pi/(delta_phi/2))):
+        x = 0.26460 - 0.035 * np.sin(phi_xmax + num * delta_phi/2)
+        y = 0.74696 + 0.035 * np.cos(phi_xmax + num * delta_phi/2)
+        z = 0.125
+        rx, ry, rz = 0.020, 3.136, 0.349
+        Ro = np.eye(3)
+        Rz = np.eye(3)
+        Ro = R.from_rotvec(np.array([rx, ry, rz])).as_matrix()
+        Rz = R.from_rotvec(np.array([0, 0, num * delta_phi/2])).as_matrix()
+        rx, ry, rz = R.from_matrix(Rz @ Ro).as_rotvec()
         discretized_poses.append([x,
             y,
-            0.124, # z
-            0.334, # rx
-            3.024, # ry
-            0.187]) # rz
+            z, # z
+            rx, # rx
+            ry, # ry
+            rz]) # rz
+
+    for num in range(int(np.pi/(delta_phi/2))):
+        x = 0.26460 + 0.035 * np.sin(phi_xmax + num * delta_phi/2)
+        y = 0.74696 + 0.035 * np.cos(phi_xmax + num * delta_phi/2)
+        z = 0.125
+        rx, ry, rz = 0.020, 3.136, 0.349# pending to determine...
+        Ro = np.eye(3)
+        Rz = np.eye(3)
+        Ro = R.from_rotvec(np.array([rx, ry, rz])).as_matrix()
+        Rz = R.from_rotvec(np.array([0, 0, -num * delta_phi/2])).as_matrix()
+        rx, ry, rz = R.from_matrix(Rz @ Ro).as_rotvec()
+        discretized_poses.append([x,
+            y,
+            z, # z
+            rx, # rx
+            ry, # ry
+            rz]) # rz
     return discretized_poses
 
 if __name__ == '__main__':
-    wpts=[[0.379,0.375,0.229,0.334,3.024,0.187],
-    [0.379,0.707,0.222,0.334,3.024,0.188],
-    [0.450, 0.779, 0.123, 0.334, 3.024, 0.187]
-    ]
+    via_points=[[0.26464, 0.41918, 0.27296, 0.034, 3.194, -0.006],
+                [0.26460, 0.74696, 0.18314, 0.033, 3.194, -0.000]]
     go_home()
     time.sleep(12)
-    print([q for q in map(util.rad2degree, get_joint_states())])
+    print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))
 
-    move_to_tcp(wpts[0])
-    time.sleep(12)
-    print([q for q in map(util.rad2degree, get_joint_states())])
+    move_tcp_to(via_points[0])
+    time.sleep(10)
+    print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))
 
-    move_to_tcp(wpts[1])
-    time.sleep(12)
-    print([q for q in map(util.rad2degree, get_joint_states())])
+    move_tcp_to(via_points[1])
+    time.sleep(10)
+    print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))
 
+    # Loop and search about the inside of  centrifuge
     place_poses = get_discretized_poses()
-    for place_pose in place_poses:
-        move_to_tcp(place_pose)
-        time.sleep(12)
-        print([q for q in map(util.rad2degree, get_joint_states())])
-        print("joint_torque:{}".format(get_joint_torques()))
-        move_to_tcp(wpts[1])
-        time.sleep(12)
-        print([q for q in map(util.rad2degree, get_joint_states())])
+    for idx, place_pose in enumerate(place_poses):
+        move_tcp_to(place_pose)
+        time.sleep(4)
+        print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))
+        tcp_force = np.array(get_tcp_force())
+        if tcp_force[2] >= -2:
+            print("There is a tube!")
+            if idx < len(place_poses)/2:
+                print("The tube is at the angle of {}".format(idx * (180 * (0.28 / np.pi))))
+            elif idx >= len(place_poses)/2:
+                print("The tube is at the angle of {}".format(-(idx-len(place_poses)/2) * 180 * (0.28 / np.pi)))
+        elif tcp_force[2] < -2:
+            print("There is nothing!")
+        move_tcp_to(via_points[1])
+        time.sleep(4)
+        print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))
+
+    move_tcp_to(via_points[0])
+    time.sleep(10)
+    print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))
 
     go_home()
     time.sleep(12)
-    print([q for q in map(util.rad2degree, get_joint_states())])
+    print("joint_states: {}".format([q for q in map(util.rad2degree, get_joint_states())]))

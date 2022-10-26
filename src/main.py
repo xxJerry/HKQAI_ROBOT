675b8814cb42defaba3cff27e846e18a58b77333
unknown <ex276@nyu.edu>
2022-10-26 17:15:25 +0800

    add main program

---
+++ src/main.py
@@ -0,0 +1,35 @@
+# 1. （waitlist）接取生成物 
+# 2.  (Done）称重, arg: 多重
+# 3. 放入离心机
+# 4. 从离心机中取出
+# 5. 将离心管放入烘干箱
+# 6. 从烘干箱取出
+
+def get_product():
+    pass
+
+def get_target_weight():
+    pass
+
+def place_tube_in_centrifuge():
+    pass
+
+def pick_tube_from_centrifuge():
+    pass
+
+def place_tube_in_oven():
+    pass
+
+def pick_tube_from_oven():
+    pass
+
+def main():
+    get_product()
+    get_target_weight()
+    place_tube_in_centrifuge()
+    pick_tube_from_centrifuge()
+    place_tube_in_oven()
+    pick_tube_from_oven()
+
+if __name__ == "main":
+    main()

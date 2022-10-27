53ab93a011dca518e418100a807e86bc8dd25486
unknown <ex276@nyu.edu>
2022-10-27 19:00:38 +0800

    Improved robustness of weight.py

--- src/balance_test.py
+++ src/balance_test.py
@@ -4,10 +4,10 @@ import serial
 def balance_serial_test(port:str, baudrate=1200, timeout=0.5):
 	ser = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
 	ser.write(b'PRINT\r')
-	weight = float(ser.read(10)[:-1])
+	weight = float(ser.readline().decode().strip()[:-1])
 	ser.close()
-	return weight
+	return weight, type(weight)
 
 
 if __name__ == '__main__':
-	print(balance_serial_test(port='com3'))
+	print(balance_serial_test(port='com3', baudrate=1200))
--- src/weight.py
+++ src/weight.py
@@ -4,38 +4,58 @@ import serial
 import time
 
 
-def get_target_weight(target: float, pump_port: str, pump_baudrate: int, \
-	bal_port: str, bal_baudrate: int):
+def get_target_weight(target: float, pump_port: str = 'COM6', \
+					  pump_baudrate: int = 9600, bal_port: str = 'COM3', bal_baudrate: int = 1200):
+	# 连接蠕动泵
 	pump = serial.Serial(port=pump_port, baudrate=pump_baudrate)
 	print("The pump is connected via port: {}".format(pump_port))
 
+	# 连接天平
 	balance = serial.Serial(port=bal_port, baudrate=bal_baudrate)
 	print("The balance is connected via port: {}".format(bal_port))
 
 	# 设置通道1的方向为顺时针
 	pump.write(b'1J\r')
-	# assert pump.read(10).decode() == '*'
+	# assert pump.readline().decode() == '*'
 
-	# 设置通道1的转速为30 rpm
-	pump.write(b'1S003000\r')
-	# assert pump.read(10).decode() == '*'
-
-	# 启动通道1
-	pump.write(b'1H\r')
+	# 设置通道1的转速为 80 rpm
+	pump.write(b'1S008000\r')
+	# assert pump.readline().decode() == '*'
 
 	speed_adjust_flag = True
+	start_pump_flag = True
 	while True:
 		balance.write(b'PRINT\r')
-		mass = float(balance.read(10)[:-1])
+		mass = float(balance.readline().decode().strip()[:-1])
 		mass_diff = target - mass
 
-		if mass_diff <= 0.1:
+		if mass_diff <= 0.03:
+			# 停止通道1
 			pump.write(b'1I\r')
 			break 
-		elif mass_diff <= mass * 0.1 and speed_adjust_flag:
-			pump.write(b'1S000500\r')
-			speed_adjust_flag = False
+		elif mass_diff <= 1:
+			if speed_adjust_flag:
+				# 设置通道1的转速为 10 rpm
+				pump.write(b'1S001000\r')
+				speed_adjust_flag = False
+			if start_pump_flag:
+				# 启动通道1
+				pump.write(b'1H\r')
+				start_pump_flag = False
 		else:
-			pass
+			if start_pump_flag:
+				# 启动通道1
+				pump.write(b'1H\r')
+			time.sleep(0.5)
+
+	# 获取完全静止后的准确质量
+	time.sleep(1)
+	balance.write(b'PRINT\r')
+	mass = float(balance.readline().decode().strip()[:-1])
+	print("The overall weight is: {}".format(mass))
+
 	pump.close()
 	balance.close()
+
+if __name__ == '__main__':
+	get_target_weight(target=50)

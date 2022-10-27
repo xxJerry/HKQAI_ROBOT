1065c445c0b424464a9c4076ced0fa9f4b8dcfaa
Xiang Xiang <xiangxiang@hkqai.hk>
2022-10-27 16:49:42 +0800

    complete main program for robots

--- src/main.py
+++ src/main.py
@@ -4,24 +4,110 @@
 # 4. 从离心机中取出
 # 5. 将离心管放入烘干箱
 # 6. 从烘干箱取出
+import socket
 
 def get_product():
-    pass
+    # TODO: pending for dev.
+    return None
 
 def get_target_weight():
-    pass
+    # TODO: pending for dev
+    return None
 
 def place_tube_in_centrifuge():
-    pass
+    HOST = "192.168.1.3"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_into_centri_left.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+
+    HOST = "192.168.1.4"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_into_centri_right.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+
 
 def pick_tube_from_centrifuge():
-    pass
+    HOST = "192.168.1.3"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_out_centri_left.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+
+    HOST = "192.168.1.4"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_out_centri_right.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+ 
 
 def place_tube_in_oven():
-    pass
+    HOST = "192.168.1.3"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_into_oven_left.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+
+    HOST = "192.168.1.4"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_into_oven_right.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
 
 def pick_tube_from_oven():
-    pass
+    HOST = "192.168.1.3"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_out_oven_left.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+
+    HOST = "192.168.1.4"
+    PORT = 29999
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((HOST, PORT))
+    tcp_command = "load f_tube_out_oven_right.urp\n"
+    tcp_socket.send(str.encode(tcp_command))
+    time.sleep(5)
+    tcp_command = "play\n"
+    tcp_socket.send(str.encode(tcp_command))
+    tcp_socket.close()
+
 
 def main():
     get_product()

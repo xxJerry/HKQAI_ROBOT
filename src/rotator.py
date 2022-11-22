64b4942025b3d6adbfc67cab46a87edfcf5f0bd2
unknown <ex276@nyu.edu>
2022-11-22 17:11:00 +0800

    Modified rotator.py

--- src/rotator.py
+++ src/rotator.py
@@ -1,126 +1,100 @@
 # -*- coding: utf-8 -*-
-import time
 
+import time
 import serial
-import modbus_tk
 import modbus_tk.defines as cst
 from modbus_tk import modbus_rtu
 
+# 注意：由于厂家硬件连接错误，导致此型号搅拌头只能使用启动(start_rotator)与停止(stop_rotator)两项功能
+# cst.READ_HOLDING_REGISTERS 代表指令0x03 （读）
+# cst.WRITE_MULTIPLE_REGISTERS 代表指令0x10 (写)
+# 1个寄存器占有两个字节
 
-class Rotator():
+
+class Rotator:
 	def __init__(self, port: str = 'com10', baudrate: int = 9600):
 		self.master = modbus_rtu.RtuMaster(serial.Serial(port=port, \
 			baudrate=baudrate,  parity=serial.PARITY_EVEN))
 		self.master.set_timeout(5.0)
 
-	# cst.READ_HOLDING_REGISTERS 代表指令0x03 （读）
-	# cst.WRITE_MULTIPLE_REGISTERS 代表指令0x10 (写)
-	# 1个寄存器占有两个字节
-
 	# 获取实际的运行时间（单位：小时 或 分钟）
 	def get_actual_runtime(self, unit_scale: str):
 		# 单位：小时
 		if unit_scale == "hour":
-			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x0A, 1)
+			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, starting_address=0x0A, quantity_of_x=1)
 			print("实际的运行时间为： {} {}s".format(read_out, unit_scale))
 		# 单位：分钟
 		else:
-			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x0B, 1)
+			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, starting_address=0x0B, quantity_of_x=1)
 			print("实际的运行时间为： {} {}s".format(read_out, unit_scale))
 		return read_out
-		
 
 	# 获取设定的运行时间（单位：小时 或 分钟）
 	def get_set_runtime(self, unit_scale: str):
 		# 单位：小时
 		if unit_scale == "hour":
-			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x22, 1)
+			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, starting_address=0x22, quantity_of_x=1)
 			print("设定的运行时间为： {} {}s".format(read_out, unit_scale))
 		# 单位：分钟
 		else:
-			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x23, 1)
+			read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, starting_address=0x23, quantity_of_x=1)
 			print("设定的运行时间为： {} {}s".format(read_out, unit_scale))
 		return read_out
 
-
 	# 获取当前实际的转速（单位：rpm）
 	def get_actual_speed(self):
-		# read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x16, 1)
-		read_out = self.master.execute(3, 0x03, 0x16, 1)
-		# self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, 0x16, output_value=[200])
+		read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, starting_address=0x16, quantity_of_x=1)
 		print("实际的转速为： {} rpm".format(read_out))
 		return read_out
-
 
 	# 获取当前设定的转速（单位：rpm）
 	def get_set_speed(self):
 		read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, starting_address=0x17, quantity_of_x=1)
-
 		print("设定的转速为： {} rpm".format(read_out))
 		return read_out
 
-
 	# 启动搅拌器
 	def start_rotator(self):
-		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, 0x1000, output_value=[0x0c])
-		read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x1000, 1)[0]
-		if read_out != 0x0c:
-			# raise Exception("Fail to start the rotator!", hex(read_out))
-			pass
-		else:
-			print("Rotator successfully started! {}".format(hex(read_out)))
-
+		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, starting_address=0x1000, output_value=[0x0c])
+		# read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x1000, 1)[0]
+		# if read_out != 0x0c:
+		# 	raise Exception("Fail to start the rotator!", read_out)
+		# else:
+		# 	print("Rotator successfully stopped! {}".format(hex(read_out)))
 
 	def stop_rotator(self):
-		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, 0x1000, output_value=[0x03])
-		read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x1000, 1)[0]
-		if read_out != 0x03:
-			# raise Exception("Fail to stop the rotator!", read_out)
-			pass
-		else:
-			print("Rotator successfully stopped! {}".format(hex(read_out)))
-
-
-	def set_speed(self, speed_value: int):
-		if speed_value > 3000 or speed_value < 0:
-			raise Exception("Wrong speed value! Please set the value in range [0, 3000]")
-		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, starting_address=0x400, output_value=[speed_value])
-		# print("set_speed: {}".format(read_out))
-		# read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x400, 1)[0]
-		# if read_out != speed_value:
-		# 	raise Exception("Fail to set the speed correctly!", hex(read_out))
-		# 	# pass
+		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, starting_address=0x1000, output_value=[0x03])
+		# read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x1000, 1)[0]
+		# if read_out != 0x03:
+		# 	raise Exception("Fail to stop the rotator!", read_out)
 		# else:
-		# 	print("Successfully set the speed! {}".format(read_out))
+		# 	print("Rotator successfully stopped! {}".format(hex(read_out)))
 
+	def set_speed(self, speed: int):
+		if speed > 3000 or speed < 0:
+			raise Exception("Wrong speed value! Please set the value in range [0, 3000]")
+		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, starting_address=0x400, output_value=[speed])
+		read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x400, 1)[0]
+		if read_out != speed:
+			raise Exception("Fail to set the speed correctly!", hex(read_out))
+		else:
+			print("Successfully set the speed! {}".format(hex(read_out)))
 
 	def set_runtime(self, runtime: int):
 		if runtime > 9000 or runtime < 0:
-			raise Exception("Wrong speed value! Please set the value in range [0, 9999]")
-		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, 0x401, output_value=[runtime])
+			raise Exception("Wrong runtime! Please set the value in range [0, 9999]")
+		self.master.execute(3, cst.WRITE_MULTIPLE_REGISTERS, starting_address=0x401, output_value=[runtime])
 		read_out = self.master.execute(3, cst.READ_HOLDING_REGISTERS, 0x401, 1)[0]
-		if read_out != speed_value:
-			# raise Exception("Fail to set the speed correctly!", read_out)
-			print("set_runtime: {}".format(read_out))
+		if read_out != runtime:
+			raise Exception("Fail to set the runtime correctly!", hex(read_out))
 		else:
-			print("Successfully set the speed! {}".format(read_out))
+			print("Successfully set the runtime! {}".format(hex(read_out)))
 
 
 if __name__ == '__main__':
 	rotator = Rotator(port='com8')
-	_ = rotator.get_set_speed()
-	_ = rotator.get_set_runtime('minute')
-	_ = rotator.get_actual_speed()
-	_ = rotator.get_actual_runtime('minute')
-	rotator.set_speed(200)
-
 	rotator.start_rotator()
-	# for i in range(64):
-	# 	print("At {}s:".format(i))
-	# 	_ = rotator.get_actual_speed()
-	# 	_ = rotator.get_set_speed()
-	# 	_ = rotator.get_actual_runtime('minute')
-	# 	_ = rotator.get_set_runtime('minute')
-	# 	time.sleep(1)
-	# time.sleep(5)
-	# rotator.stop_rotator()
+	print("Rotator started!")
+	time.sleep(5)
+	rotator.stop_rotator()
+	print("Rotator stopped!")
